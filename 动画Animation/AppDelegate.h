b1e452ed2ca4457c771b44e8aff75a8071eb414c
wuyukobe24 <jkwxq@sina.com>
2017-08-02 14:56:21 +0800

    Initial Commit

---
+++ 动画Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  动画Animation
+//
+//  Created by WangXueqi on 17/8/2.
+//  Copyright © 2017年 JingBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 动画Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  动画Animation
+//
+//  Created by WangXueqi on 17/8/2.
+//  Copyright © 2017年 JingBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
